23a3cccdb6b14d736fe4cac7df7f347e0afb196d
dominguerilla <dominguerilla@users.noreply.github.com>
2016-03-14 12:53:04 -0400

    Added Accessory.cs

---
+++ Assets/game-science/Assets/Accessories/Accessory.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using TreeSharpPlus;
+
+public abstract class Accessory : SmartObject {
+
+    /**
+     * A simple function to rotate the Accessory while it is on the ground unequipped.
+     */ 
+    public virtual void IdleRotate(Transform obj, float speed)
+    {
+        obj.Rotate(Vector3.up * Time.deltaTime * speed);
+    }
+
+
+    /**
+     * The subtree that happens when a Toy decides to 'use' the Accessory.
+     * For equippable items, this would entail making the object display on the Toy, changing the state of both the Toy and Accessory, and then destroying the Accessory from the game world.
+     * Make the subtree execute successfully if equipping is successful, otherwise make it fail if it cannot be equipped at the time.
+     * 
+     */
+    public abstract Node OnUse();
+}
---
+++ Assets/game-science/Assets/Accessories/SmartAxe.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using TreeSharpPlus;
+
+public class SmartAxe : Accessory {
+    
+    public override string Archetype { get { return "SmartAxe"; } }
+    public float RotateSpeed = 100.0f;
+
+    private bool isEquipped; //Whether or not this item is equipped
+
+	void Start () {
+        isEquipped = false;
+	}
+
+    void Update()
+    {
+        IdleRotate(transform, RotateSpeed);
+    }
+
+    public override Node OnUse()
+    {
+        return new Sequence(); //placeholder
+    }
+}
