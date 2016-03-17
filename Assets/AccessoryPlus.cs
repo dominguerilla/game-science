5879e5705f727efbc038c7c561516810ffaf5b5b
geoxile <jsj57@scarletmail.rutgers.edu>
2016-03-17 01:51:07 -0400

    added AccessoryPlus, a generic type of accessory whose function you can set using a gameobject containing functions of return type void and no parameters

---
+++ Assets/AccessoryPlus.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using TreeSharpPlus;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+[RequireComponent (typeof(SphereCollider))]
+public class AccessoryPlus : Accessory
+{
+
+    public GameObject equipModel;
+    public float RotateSpeed = 100.0f;
+    public string type;
+
+    public override string Archetype { get { return type; } }
+    public override bool IsEquippable { get { return true; } }
+    public override GameObject EquipModel { get { return equipModel; } }
+    public override EquipSlots EquipSlot { get { return EquipSlots.RightHand; } }
+
+    public UnityEvent callme;
+
+    void Update()
+    {
+        IdleRotate(transform, RotateSpeed);
+    }
+
+    public override Node OnUse(Toy toy)
+    {
+        return new Sequence(
+            new LeafAssert(() => { return IsEquippable; }),
+            new LeafAssert(() => { return toy.GetAvailableSlotCount() > 0; }),
+            new EquipAccessory(toy, this, EquipSlot),
+            new LeafInvoke(() => { GameObject.Destroy(this); })
+            );
+    }
+
+    public override Node ToyUse(Toy toy)
+    {
+
+        return null;
+    }
+    
+}
