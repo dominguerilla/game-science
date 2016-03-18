af37d626a7959c978fe1e81c633d387484069e49
Hi-Fu-Mi <jungchoe@scarletmail.rutgers.edu>
2016-03-18 15:58:24 -0400

    Updated GUI. Assets will spawn whereever you click your mouse. Left click for toy, right click for accessory.

--- Assets/game-science/Assets/Scripts/GUI Scripts/PlaceAccessory.cs
+++ Assets/game-science/Assets/Scripts/GUI Scripts/PlaceAccessory.cs
@@ -32,4 +32,31 @@ public class PlaceAccessory : MonoBehaviour {
         }
     }
 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            //gonna need a fix. We have to manually set the z according to the main camera. Need some sort of adaptive coordinate thingy
+            Vector3 point = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 45f));
+            switch (accessoryDropdown.value)
+            {
+                case 0:
+                    Instantiate(axePrefab, new Vector3(point.x, point.y, point.z), Quaternion.identity);
+                    break;
+                case 1:
+                    Instantiate(speedPotionPrefab, new Vector3(point.x, point.y, point.z), Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(woodBagPrefab, new Vector3(point.x, point.y, point.z), Quaternion.identity);
+                    break;
+                case 3:
+                    Instantiate(santaHatPrefab, new Vector3(point.x, point.y, point.z), Quaternion.identity);
+                    break;
+                default:
+                    Instantiate(axePrefab, new Vector3(point.x, point.y, point.z), Quaternion.identity);
+                    break;
+            }
+        }
+    }
+
 }
--- Assets/game-science/Assets/Scripts/GUI Scripts/PlaceToy.cs
+++ Assets/game-science/Assets/Scripts/GUI Scripts/PlaceToy.cs
@@ -30,6 +30,37 @@ public class PlaceToy : MonoBehaviour {
                 Instantiate(lumberjackPrefab);
                 break;
         }
+
     }
+
+
+    void Update ()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //gonna need a fix. We have to manually set the z according to the main camera. Need some sort of adaptive coordinate thingy
+            Vector3 point = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 45f));
+            switch (toyDropdown.value)
+            {
+                case 0:
+                    Instantiate(lumberjackPrefab, new Vector3(point.x,point.y,point.z),Quaternion.identity);
+                    break;
+                case 1:
+                    Instantiate(skeletonPrefab, new Vector3(point.x, point.y, point.z), Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(unityChanPrefab, new Vector3(point.x, point.y, point.z), Quaternion.identity);
+                    break;
+                case 3:
+                    Instantiate(trollPrefab, new Vector3(point.x, point.y, point.z), Quaternion.identity);
+                    break;
+                default:
+                    Instantiate(lumberjackPrefab, new Vector3(point.x, point.y, point.z), Quaternion.identity);
+                    break;
+            }
+        }
+    }
+
+
 
 }
