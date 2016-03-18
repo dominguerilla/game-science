b896b8472edb6046e734372e981f0749cb2e5544
Hi-Fu-Mi <jungchoe@scarletmail.rutgers.edu>
2016-03-18 17:15:10 -0400

    Added ScrollWheel implementation to GUI

--- Assets/game-science/Assets/Scripts/GUI Scripts/PlaceAccessory.cs
+++ Assets/game-science/Assets/Scripts/GUI Scripts/PlaceAccessory.cs
@@ -10,6 +10,9 @@ public class PlaceAccessory : MonoBehaviour {
     public GameObject santaHatPrefab;
     public Dropdown accessoryDropdown;
 
+    int end = 3; //number of accessories
+    int current = 0;
+
     public void onClick()
     {
         switch (accessoryDropdown.value)
@@ -34,8 +37,36 @@ public class PlaceAccessory : MonoBehaviour {
 
     void Update()
     {
+
+        //switching accessories with scroll wheel
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if(current > 0)
+            {
+                current--;
+            }
+            else
+            {
+                current = end;
+            }
+        }else if(Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if(current < end)
+            {
+                current++;
+            }
+            else
+            {
+                current = 0;
+            }
+        }
+
+        //System.Console.WriteLine(current);
+        accessoryDropdown.value = current;
+
         if (Input.GetMouseButtonDown(1))
         {
+
             //gonna need a fix. We have to manually set the z according to the main camera. Need some sort of adaptive coordinate thingy
             Vector3 point = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 45f));
             switch (accessoryDropdown.value)
