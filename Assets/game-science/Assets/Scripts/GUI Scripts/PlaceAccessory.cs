375ea1b9d88bfa40380f9101fc761386033640c4
Colin Dru <colindru@gmail.com>
2016-03-17 21:40:21 -0400

    Made simple spawning UI in TestScenes>UITownParty

---
+++ Assets/game-science/Assets/Scripts/GUI Scripts/PlaceAccessory.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlaceAccessory : MonoBehaviour {
+
+    public GameObject axePrefab;
+    public GameObject speedPotionPrefab;
+    public GameObject woodBagPrefab;
+    public Dropdown accessoryDropdown;
+
+    public void onClick()
+    {
+        switch (accessoryDropdown.value)
+        {
+            case 0:
+                Instantiate(axePrefab);
+                break;
+            case 1:
+                Instantiate(speedPotionPrefab);
+                break;
+            case 2:
+                Instantiate(woodBagPrefab);
+                break;
+            default:
+                Instantiate(axePrefab);
+                break;
+        }
+    }
+
+}
---
+++ Assets/game-science/Assets/Scripts/GUI Scripts/PlaceToy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlaceToy : MonoBehaviour {
+
+    public GameObject lumberjackPrefab;
+    public GameObject skeletonPrefab;
+    public GameObject unityChanPrefab;
+    public Dropdown toyDropdown;
+
+    public void onClick()
+    {
+        switch (toyDropdown.value)
+        {
+            case 0:
+                Instantiate(lumberjackPrefab);
+                break;
+            case 1:
+                Instantiate(skeletonPrefab);
+                break;
+            case 2:
+                Instantiate(unityChanPrefab);
+                break;
+            default:
+                Instantiate(lumberjackPrefab);
+                break;
+        }
+    }
+
+}
