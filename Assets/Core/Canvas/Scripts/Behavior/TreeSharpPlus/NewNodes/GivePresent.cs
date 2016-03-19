51bea4070cffe069743292c8a76ce22e301dd074
Colin Dru <colindru@gmail.com>
2016-03-19 18:28:23 -0400

    Finished SantaHat, fixed WalkToNearestChar

---
+++ Assets/Core/Canvas/Scripts/Behavior/TreeSharpPlus/NewNodes/GivePresent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TreeSharpPlus
+{
+    public class GivePresent : Node
+    {
+        Toy gifter;
+        GameObject present;
+
+        public GivePresent(Toy gifter, GameObject present)
+        {
+            this.gifter = gifter;
+            this.present = present;
+        }
+
+        public override IEnumerable<RunStatus> Execute()
+        {
+            // Play attack animation
+            gifter.GetAnimator().SetTrigger("Attack");
+            // Give present
+            UnityEngine.Object.Instantiate(present,
+                gifter.transform.position,
+                new Quaternion(0,0,0,0));
+            yield return RunStatus.Success;
+        }
+    }
+}
---
+++ Assets/game-science/Assets/Scripts/Other Scripts/GUI Scripts/PlaceToy.cs
@@ -0,0 +1,114 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlaceToy : MonoBehaviour {
+
+    public GameObject lumberjackPrefab;
+    public GameObject skeletonPrefab;
+    public GameObject unityChanPrefab;
+    public GameObject trollPrefab;
+    public Dropdown toyDropdown;
+
+    int toycurrent = 0;
+    int totalToys = 3; //total-1 since we start at 0
+
+    public void onClick()
+    {
+        switch (toyDropdown.value)
+        {
+            case 0:
+                Instantiate(lumberjackPrefab);
+                break;
+            case 1:
+                Instantiate(skeletonPrefab);
+                break;
+            case 2:
+                Instantiate(unityChanPrefab);
+                break;
+            case 3:
+                Instantiate(trollPrefab);
+                break;
+            default:
+                Instantiate(lumberjackPrefab);
+                break;
+        }
+
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetKeyDown("1"))
+        {
+            if (toycurrent > 0)
+            {
+                toycurrent--;
+            }
+            else
+            {
+                toycurrent = totalToys;
+            }
+        }
+        else if (Input.GetKeyDown("2"))
+        {
+            if (toycurrent < totalToys)
+            {
+                toycurrent++;
+            }
+            else
+            {
+                toycurrent = 0;
+            }
+        }
+
+        /*if (Input.GetKeyDown("1"))
+        {
+            toycurrent = 0;
+        }
+        else if (Input.GetKeyDown("2"))
+        {
+            toycurrent = 1;
+        }
+        else if (Input.GetKeyDown("3"))
+        {
+            toycurrent = 2;
+        }
+        else if (Input.GetKeyDown("4"))
+        {
+            toycurrent = 3;
+        }*/
+
+
+        toyDropdown.value = toycurrent;
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //gonna need a fix. We have to manually set the z according to the main camera. Need some sort of adaptive coordinate thingy
+            Vector3 point = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 45f));
+            switch (toyDropdown.value)
+            {
+                case 0:
+                    Instantiate(lumberjackPrefab, new Vector3(point.x,point.y,point.z),Quaternion.identity);
+                    break;
+                case 1:
+                    Instantiate(skeletonPrefab, new Vector3(point.x, point.y, point.z), Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(unityChanPrefab, new Vector3(point.x, point.y, point.z), Quaternion.identity);
+                    break;
+                case 3:
+                    Instantiate(trollPrefab, new Vector3(point.x, point.y, point.z), Quaternion.identity);
+                    break;
+                default:
+                    Instantiate(lumberjackPrefab, new Vector3(point.x, point.y, point.z), Quaternion.identity);
+                    break;
+            }
+        }
+    }
+
+
+
+}
